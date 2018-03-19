a0459ec3f2f979f87640af0971f3bf24a5ed3d5f
dawangong <dawangong@126.com>
2018-03-19 17:57:05 +0800

    demo

--- modal/js/modal.js
+++ modal/js/modal.js
@@ -22,8 +22,17 @@ class Modal {
 	}
 
 	getElement() {
-		this.contain = this.$('.' + this.config.contain);
-		this.body = this.$('body');
+		this.contain = this.$(document, '.' + this.config.contain);
+		this.body = this.$(document, 'body');
+		this.getOther();
+	}
+
+	getOther(){
+		let ele = ['header', 'footer', 'title', 'close', 'decide', 'cancel'];
+		let parents = ['contain', 'contain', 'header', 'header', 'footer', 'footer'];
+		ele.forEach((v, i) => {
+			this[v] = this.$(this[parents[i]], '.modal-'+ v);
+		})
 	}
 
 	setElement() {
@@ -39,10 +48,55 @@ class Modal {
 			background: 'white'
 		});
 		this.css(this.body, {background: '#7f7f7f'});
+		this.css(this.header, {width: '100%', height: '35px', background: '#5998df'});
+		this.css(this.title, {
+			float: 'left',
+			height: '100%',
+			lineHeight: '35px',
+			fontSize: '18px',
+			paddingLeft: '5px',
+			color: 'white'
+		});
+		this.css(this.close, {
+			float: 'right',
+			cursor: 'pointer',
+			height: '100%',
+			lineHeight: '35px',
+			fontSize: '20px',
+			paddingRight: '8px',
+			color: 'white'
+		});
+		this.css(this.footer, {
+			background: '#f5f5f5',
+			width: '100%',
+			height: '40px',
+			position: 'absolute',
+			left: 0,
+			bottom: 0
+		});
+		this.css(this.decide, {
+			position: 'absolute',
+			background: '#5998df',
+			right: '65px',
+			bottom: '6px',
+			padding: '3px 6px',
+			fontSize: '16px',
+			color: 'white',
+			cursor: 'pointer'
+		});
+		this.css(this.cancel, {
+			position: 'absolute',
+			background: 'white',
+			right: '10px',
+			bottom: '6px',
+			padding: '3px 6px',
+			fontSize: '16px',
+			cursor: 'pointer'
+		});
 	}
 
-	$(obj) {
-		return document.querySelector(obj);
+	$(parent, obj) {
+		return parent.querySelector(obj);
 	}
 
 	css(obj, option) {
