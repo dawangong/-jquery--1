e81c918a7de597b9fe7a7179588ccf696682f8b8
dawangong <dawangong@126.com>
2018-08-10 14:33:04 +0800

    :fire: 为实现翻页重新生成 改变架构 初次就动态生成

--- 日期选择器/js/datePicker.js
+++ 日期选择器/js/datePicker.js
@@ -9,25 +9,51 @@ class datePicker {
         };
         this.itemLevel1 = ['bg-datePick-input', 'bg-datePick-content', 'bg-datePick-icon'];
         this.itemLevel2 = ['bg-datePick-last-year', 'bg-datePick-last-month', 'bg-datePick-year-value', 'bg-datePick-month-value', 'bg-datePick-next-month', 'bg-datePick-next-year'];
-        this.item = ['lastYear', 'lastMonth', 'yearValue', 'monthValue', 'nextMonth', 'nextYear'];
-        this.itemLevel3 = ['bg-datePick-header', 'bg-datePick-table', 'bg-datePick-bar'];
+        this.itemLevel3 = ['bg-datePick-bar', 'bg-datePick-table'];
+        this.item3 = ['div', 'table'];
+        this.item1 = ['lastYear', 'lastMonth', 'yearValue', 'monthValue', 'nextMonth', 'nextYear'];
         this.extend(config);
-        this.setStyle(false);
+        let date = new Date();
+        let month;
+        if (0 < date.getMonth() && date.getMonth() < 9) {
+            month = `0${date.getMonth() + 1}`;
+        } else {
+            month = `${date.getMonth() + 1}`;
+        }
+        this.year = date.getFullYear();
+        this.month = month;
+        this.setStyle();
     }
 
-    getElement() {
+    setElement() {
         this.container = this.$(document, this.config.container);
         this.itemLevel1.forEach(v => {
             let str = this.getConst(v);
             this[str] = this.$(this.container, v);
+        });
+        this.itemLevel3.forEach((v, i) => {
+            let str = this.getConst(v);
+            this.createEle(this.item3[i], str, this.content);
+            this[str].className = v;
         });
         this.itemLevel3.forEach(v => {
             let str = this.getConst(v);
             this[str] = this.$(this.content, v);
         });
         this.itemLevel2.forEach((v, i) => {
-            let str = this.getConst(v);
-            this[this.item[i]] = this.$(this.bar, v);
+            this.createEle('span', this.item1[i], this.bar);
+            this[this.item1[i]].className = v;
+        });
+        this.itemLevel2.forEach((v, i) => {
+            this[this.item1[i]] = this.$(this.bar, v);
+        });
+        let itemLevel4 = ['iconfont icon-houtui', 'iconfont icon-shangyiye', 'iconfont icon-xiayiye', 'iconfont icon-qianjin'];
+        let item4 = ['lastYear', 'lastMonth', 'nextMonth', 'nextYear'];
+        let sp = [];
+        itemLevel4.forEach((v, i) => {
+            sp[i] = document.createElement('span');
+            sp[i].className = v;
+            this[item4[i]].appendChild(sp[i]);
         });
     }
 
@@ -44,43 +70,53 @@ class datePicker {
         this.nextYear.addEventListener('click', () => {
             this.getNextYear();
         }, false);
-        this.input.addEventListener('focus',() => {
+        this.input.addEventListener('focus', () => {
             this.css(this.content, {
                 display: 'block'
             });
-        } ,false);
+        }, false);
         this.trWeek.forEach(v => {
             v.addEventListener('mouseover', (ev) => {
                 this.css(ev.target, {
                     color: '#009eff'
                 })
-            },false);
+            }, false);
             v.addEventListener('mouseout', (ev) => {
                 this.css(ev.target, {
                     color: 'rgb(96, 98, 102)'
                 })
-            },false);
+            }, false);
             v.addEventListener('click', (ev) => {
-                this.today = ev.target.innerText;
+                let today = ev.target.innerText;
+                if (0 < today && today < 9) {
+                    this.today = `0${ev.target.innerText}`;
+                } else {
+                    this.today = today;
+                }
                 this.input.value = `${this.year}-${this.month}-${this.today}`;
                 this.css(this.content, {
                     display: 'none'
                 })
-            },false);
+            }, false);
         });
         this.bar.addEventListener('mouseover', (ev) => {
             this.css(ev.target, {
                 color: '#009eff'
             })
-        },false)
+        }, false);
         this.bar.addEventListener('mouseout', (ev) => {
             this.css(ev.target, {
                 color: 'rgb(96, 98, 102)'
             })
-        },false)
+        }, false);
     }
 
-    createAp(tag, obj, text, complex = false, index = null) {
+    createEle(ele, rename, obj) {
+        this[rename] = document.createElement(ele);
+        obj.appendChild(this[rename]);
+    }
+
+    createAp(tag, obj, text = '', complex = false, index = null) {
         if (!complex) {
             this[tag] = document.createElement(tag);
             obj.appendChild(this[tag]);
@@ -151,9 +187,6 @@ class datePicker {
                 suoyin++;
             }
         });
-        let date = new Date();
-        this.year = date.getFullYear();
-        this.month = date.getMonth() + 1;
         this.yearValue.innerText = `${this.year}年`;
         this.css(this.yearValue, {
             fontSize: '16px',
@@ -163,7 +196,8 @@ class datePicker {
             cursor: 'pointer',
             color: '#606266'
         });
-        this.monthValue.innerText = `${this.month}月`;;
+        this.monthValue.innerText = `${this.month}月`;
+        ;
         this.css(this.monthValue, {
             fontSize: '16px',
             fontWeight: 500,
@@ -174,75 +208,69 @@ class datePicker {
         });
     }
 
-    setStyle(isUpdate) {
-        if(!isUpdate) {
-            this.getElement();
-            this.css(this.content, {
-                width: '300px',
-                border: '1px solid #e3e7ec',
-                boxSizing: 'border-box',
-                borderRadius: '2px',
-                marginTop: '5px',
-                display: 'none'
-            });
-            this.css(this.input, {
-                width: '150px',
-                height: '20px',
-                textIndent: '2em',
-                outline: 'none'
-            });
-            this.css(this.icon, {
-                position: 'absolute',
-                left: '12px',
-                top: '10px'
-            });
-            this.css(this.bar, {
-                width: '270px',
-                margin: '5px auto',
-                display: 'flex',
-                justifyContent: 'space-around',
-            });
-            this.css(this.table, {
-                borderCollapse: 'collapse',
-                margin: '0 auto'
-            });
-            let lastYear = this.lastYear.getElementsByTagName('span')[0];
-            this.css(lastYear, {
-                fontSize: '14px',
-                position: 'relative',
-                top: '-1px',
-                color: '#303133',
-                cursor: 'pointer'
-            });
-            let nextYear = this.nextYear.getElementsByTagName('span')[0];
-            this.css(nextYear, {
-                fontSize: '14px',
-                position: 'relative',
-                top: '-1px',
-                color: '#303133',
-                cursor: 'pointer'
-            });
-            let lastMonth = this.lastMonth.getElementsByTagName('span')[0];
-            this.css(lastMonth, {
-                color: '#303133',
-                cursor: 'pointer'
-            });
-            let nextMonth = this.nextMonth.getElementsByTagName('span')[0];
-            this.css(nextMonth, {
-                color: '#303133',
-                cursor: 'pointer'
-            });
-        }
+    setStyle() {
+        this.setElement();
+        this.css(this.content, {
+            width: '300px',
+            border: '1px solid #e3e7ec',
+            boxSizing: 'border-box',
+            borderRadius: '2px',
+            marginTop: '5px',
+            display: 'none'
+        });
+        this.css(this.input, {
+            width: '150px',
+            height: '20px',
+            textIndent: '2em',
+            outline: 'none'
+        });
+        this.css(this.icon, {
+            position: 'absolute',
+            left: '12px',
+            top: '10px'
+        });
+        this.css(this.bar, {
+            width: '270px',
+            margin: '5px auto',
+            display: 'flex',
+            justifyContent: 'space-around',
+        });
+        this.css(this.table, {
+            borderCollapse: 'collapse',
+            margin: '0 auto'
+        });
+        let lastYear = this.lastYear.getElementsByTagName('span')[0];
+        this.css(lastYear, {
+            fontSize: '14px',
+            position: 'relative',
+            top: '-1px',
+            color: '#303133',
+            cursor: 'pointer'
+        });
+        let nextYear = this.nextYear.getElementsByTagName('span')[0];
+        this.css(nextYear, {
+            fontSize: '14px',
+            position: 'relative',
+            top: '-1px',
+            color: '#303133',
+            cursor: 'pointer'
+        });
+        let lastMonth = this.lastMonth.getElementsByTagName('span')[0];
+        this.css(lastMonth, {
+            color: '#303133',
+            cursor: 'pointer'
+        });
+        let nextMonth = this.nextMonth.getElementsByTagName('span')[0];
+        this.css(nextMonth, {
+            color: '#303133',
+            cursor: 'pointer'
+        });
         this.createAp('tbody', this.table);
         this.initDate();
         this.css(this.trContain[0], {
             borderBottom: '1px solid #e3e7ec'
         });
         this.bindEvent();
-    }
-
-    extend(config) {
-        Object.assign(this.config, config);
     }
 
     isRun() {
@@ -254,15 +282,8 @@ class datePicker {
 
     cacl() {
         let dateNum = [31, this.isRun(), 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
-        let date = new Date();
-        let year = date.getFullYear();
-        let month;
-        if (0 < date.getMonth() && date.getMonth() < 9) {
-            month = `0${date.getMonth() + 1}`;
-        } else {
-            month = `${date.getMonth() + 1}`;
-        }
-
+        let year = this.year;
+        let month = this.month;
         let weekNum = new Date(`${year},${month},01`).getDay();
         let index = 1;
         let arr = [];
@@ -287,29 +308,39 @@ class datePicker {
     }
 
     getLastMonth() {
-        this.table.remove(this.tbody);
-        this.setStyle(true);
+        this.table.remove(this.table);
+        console.log(this.month);
+        this.month = `0${this.month - 1}`;
+        console.log(this.month);
+        this.setStyle();
     }
 
     getNextMonth() {
-        this.table.remove(this.tbody);
-        this.setStyle(true);
+        this.table.remove(this.table);
+        this.month = `0${this.month + 1}`;
+        this.setStyle();
     }
 
     getLastYear() {
-        this.table.remove(this.tbody);
-        this.setStyle(true);
+        this.table.remove(this.table);
+        this.year = `0${this.year - 1}`;
+        this.setStyle();
     }
 
     getNextYear() {
-        this.table.remove(this.tbody);
-        this.setStyle(true);
+        this.table.remove(this.table);
+        this.year = `0${this.year + 1}`;
+        this.setStyle();
     }
 
     getConst(str) {
         let index = str.lastIndexOf('-');
         let res = str.substring(index + 1);
         return res;
+    }
+
+    extend(config) {
+        Object.assign(this.config, config);
     }
 
     $(parent, obj) {
