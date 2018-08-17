55d3a8e81a57019df523dddd8cd35a83906046c9
dawangong <dawangong@126.com>
2018-08-17 20:00:23 +0800

    提示初步完成

--- 消息提示/js/Message.js
+++ 消息提示/js/Message.js
@@ -2,12 +2,13 @@ class Message {
     constructor(config = {}) {
         this.config = {
             container: 'bg-message-container',
-            message: 'example',
-            type: 'tishi'
+            message: '这是一段测试提示语',
+            type: 'jinggao'
         };
         this.extend(config);
         this.init()
     }
+
     init() {
         this.getElement();
         this.setStyle();
@@ -17,31 +18,90 @@ class Message {
     getElement() {
         this.container = this.$(document, 'bg-message-container');
         this[this.config.type] = this.createElement(this.container, 'span', `iconfont icon-${this.config.type}`);
-        this.message = this.createElement(this.container, 'span', 'bg-message');
+        this.message = this.createElement(this.container, 'span', 'bg-message-message');
+        this.close = this.createElement(this.container, 'span', 'bg-message-close');
     }
 
     createElement(parent, tagName, className) {
         let tempNode = document.createElement(tagName);
         tempNode.className = className;
-        parent.parentNode.appendChild(tempNode);
+        parent.appendChild(tempNode);
+        return tempNode;
     }
 
     setStyle() {
+        this.css(this.container, {
+            position: 'fixed',
+            left: '50%',
+            top: 20,
+            padding: '0px 40px',
+            transform: 'translate(-50%)',
+            borderRadius: '5px',
+            height: '35px',
+            border: `1px solid ${this.recognize(this.config.type).borderColor}`,
+            background: this.recognize(this.config.type).bgColor
+        });
         this.css(this[this.config.type], {
+            position: 'absolute',
+            left: 0,
             padding: '0 10px',
-            color: this.recognize(this.config.type)
+            height: '35px',
+            lineHeight: '35px',
+            fontSize: '22px',
+            color: this.recognize(this.config.type).color
+        });
+        this.message.innerText = this.config.message;
+        this.css(this.message, {
+            display: 'inline-block',
+            fontSize: '14px',
+            height: '35px',
+            lineHeight: '35px',
+            color: this.recognize(this.config.type).color
+        });
+        this.close.innerText = '×';
+        this.css(this.close, {
+            position: 'absolute',
+            top: '3px',
+            right: '10px',
+            cursor: 'pointer',
+            fontSize: '18px',
+            color: this.recognize(this.config.type).color
         })
     }
 
     recognize(type) {
-        let color;
+        let theme = {};
         switch (type) {
-            case 'tishi': color = 'red';break;
-            case 'jinggao': color = 'red';break;
-            case 'chenggong': color = 'red';break;
-            case 'shibai': color = 'red';break;
+            case 'tishi':
+                theme = {
+                    color: '#909399',
+                    bgColor: '#edf2fc',
+                    borderColor: '#ebeef5'
+                };
+                break;
+            case 'jinggao':
+                theme = {
+                    color: '#e6a23c',
+                    bgColor: '#fdf6ec',
+                    borderColor: '#faecd8'
+                };
+                break;
+            case 'chenggong':
+                theme = {
+                    color: '#67c23a',
+                    bgColor: '#f0f9eb',
+                    borderColor: '#e1f3d8'
+                };
+                break;
+            case 'shibai':
+                theme = {
+                    color: '#f56c6c',
+                    bgColor: '#fef0f0',
+                    borderColor: '#fde2e2'
+                };
+                break;
         }
-        return color;
+        return theme;
     }
 
     setPosition() {
